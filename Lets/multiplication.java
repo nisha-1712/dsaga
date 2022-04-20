41d3413c08aa7f31080614fadf1199e1b17890d1
Nisha Bhavsar <nisha.bhavsar17@gmail.com>
2022-04-20 16:44:55 +0530

    blocks

---
+++ Lets/multiplication.java
@@ -0,0 +1,15 @@
+class Multiplication{
+	public static void main(String[] args){
+		//Multiplication
+	int num1 = 40;
+	int num2 = 30;
+	int num3 = 20;
+	int num4 = 10;
+	int num5= num1 * num2 * num3 * num4;
+	System.out.println("Multiplication:" + num5);
+	}
+	
+}
+	
+
+	
